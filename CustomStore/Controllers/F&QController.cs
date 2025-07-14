7eb70ad5a9607e6020d422cec99323c82bf4ec18
Phong111111111 <phongnsph49214@gmail.com>
2025-07-14 15:47:42 +0700

    first commit

---
+++ CustomStore/Controllers/F&QController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomStore.Controllers
+{
+    [ApiController]
+    [Route("F&Q")]
+    public class F_QController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
